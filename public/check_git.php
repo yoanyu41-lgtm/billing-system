66737cb10ca0d662490734c05769464f28d023b2
Yoan Yu <yoanyu41@gmail.com>
2026-07-16 19:11:34 +0700

    chore: clean up all ad-hoc test, debug and scratch files

--- public/check_git.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "=== GIT STATUS ===\n";
-echo shell_exec('git status 2>&1') . "\n";
-echo "=== GIT LOG ===\n";
-echo shell_exec('git log -n 3 2>&1') . "\n";
